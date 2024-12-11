f5570d479a9313d3d303c2dd99c462bf2ac162f4
Bo Vagner Hansen <bvh@imada.sdu.dk>
2024-12-11 10:02:41 +0100

    Add ggplot of metabolite distributions

--- R/functions.R
+++ R/functions.R
@@ -4,18 +4,30 @@
 #'
 #' @return A data.frame/tibble.
 descriptive_stats <- function(df) {
-    out <- df |>
-        dplyr::group_by(metabolite) |>
-        dplyr::summarise(across(
-            value,
-            list(
-                mean = mean,
-                sd = sd
-            )
-        )) |>
-        dplyr::mutate(across(
-            tidyselect::where(is.numeric),
-            ~ round(.x, digits = 1)
-        ))
-    return(out)
+  out <- df |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(across(
+      value,
+      list(
+        mean = mean,
+        sd = sd
+      )
+    )) |>
+    dplyr::mutate(across(
+      tidyselect::where(is.numeric),
+      ~ round(.x, digits = 1)
+    ))
+  return(out)
+}
+
+#' Plot distribution of each metabolite
+#'
+#' @param df A data.frame/tibble.
+#'
+#' @return A ggplot2 plot object
+plot_distributions <- function(df) {
+  p <- ggplot2::ggplot(df, aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(vars(metabolite), scales = "free")
+  return(p)
 }
